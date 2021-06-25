91a50bfd8b6afbfea8400c3e14fdedbf70810495
Meta Code <mtcd.net@gmail.com>
2021-06-25 12:45:43 +0300

    Generate layout container

---
+++ libs/layout/src/lib/containers/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mtcd-net-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
