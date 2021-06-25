97177edd8090d117ed28e2293c9f97426dbf64d9
Meta Code <mtcd.net@gmail.com>
2021-06-25 14:30:58 +0300

    [WIP] Refactor toolbar presentational component

--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './containers/layout/layout.component';
 import { MaterialModule } from '@mtcd-net/material';
 import { RouterModule } from '@angular/router';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule, RouterModule],
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, ToolbarComponent],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
