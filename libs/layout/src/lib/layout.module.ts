9a5726dfc1f7f8e8aa470f0e07796e69c10220e7
Meta Code <mtcd.net@gmail.com>
2021-06-29 15:20:35 +0300

    Extract header and footer components

--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -4,10 +4,12 @@ import { LayoutComponent } from './containers/layout/layout.component';
 import { MaterialModule } from '@mtcd-net/material';
 import { RouterModule } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule, RouterModule],
-  declarations: [LayoutComponent, ToolbarComponent],
+  declarations: [LayoutComponent, ToolbarComponent, HeaderComponent, FooterComponent],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
