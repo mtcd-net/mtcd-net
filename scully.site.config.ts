37c5d80d33e34deac2fc6420f6121e0972df855c
Meta Code <mtcd.net@gmail.com>
2021-07-01 15:57:07 +0300

    Configure scully and ignore artefacts

--- scully.site.config.ts
+++ scully.site.config.ts
@@ -1,8 +1,8 @@
 import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
-  projectRoot: "./apps/site/src",
-  projectName: "site",
+  puppeteerLaunchOptions: { executablePath: 'chromium' },
+  projectRoot: './apps/site/src',
+  projectName: 'site',
   outDir: './dist/static',
-  routes: {
-  }
+  routes: {},
 };
