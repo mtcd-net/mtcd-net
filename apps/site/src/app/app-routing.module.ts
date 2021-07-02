cee3eed9d0c42b55bdc46f1585e9fddb75edfe5e
Meta Code <mtcd.net@gmail.com>
2021-07-02 21:17:57 +0300

    Eliminate unused declarations

--- apps/site/src/app/app-routing.module.ts
+++ apps/site/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { CareersComponent } from './pages/careers/careers.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'careers', component: CareersComponent },
--- apps/site/src/app/app.module.ts
+++ apps/site/src/app/app.module.ts
@@ -8,14 +8,10 @@ import { MaterialModule } from '@mtcd-net/material';
 import { LayoutModule } from '@mtcd-net/layout';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { CareersComponent } from './pages/careers/careers.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, ServicesComponent, ProductsComponent, CareersComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
